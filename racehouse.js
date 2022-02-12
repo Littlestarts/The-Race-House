1d9e4bb3839b768b0c90d9899ba35da49676ea40
Sheba Bacot <sheba.bacot@gmail.com>
2022-02-12 09:40:37 -0500

    added image, code and text

---
+++ racehouse.js
@@ -0,0 +1,9 @@
+<script>
+function mouseOver() {
+  document.getElementById("nav").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("nav").style.color = "black";
+}
+</script>
